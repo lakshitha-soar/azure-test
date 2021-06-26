6670b72ff86d92ae0d60d6debd2d490417a7fce3
Lakshitha Madushan <lakshitha199412@gmail.com>
2021-06-26 14:50:25 +0530

    first commit

---
+++ src/main/java/com/example/demonew/DemonewApplication.java
@@ -0,0 +1,21 @@
+package com.example.demonew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemonewApplication {
+
+	@GetMapping("/message")
+	public String message() {
+		return "Successfully done - buddy !!!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonewApplication.class, args);
+	}
+
+}
